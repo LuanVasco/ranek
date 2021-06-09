ee74b5f09a218830b38c1a06f0c7c695161911ba
LuanVasco <devluangodoy@gmail.com>
2021-06-08 23:25:21 -0300

    Adicionando api e component produtos

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="produtos-container">
+    <article v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos " :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="title">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      produtos: null,
+    }
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto")
+      .then(response => response.json())
+      .then(response => {
+        this.produtos = response;
+      })
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="home">
-  </div>
+  <section>
+    <h1>👍 Compre ou Venda 🤙</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar"
+import ProdutosLista from "@/components/ProdutosLista"
 export default {
-  name: 'Home',
+  name: "Home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  }
 }
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87F;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>

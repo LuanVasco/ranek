ace5f48bf47d7a6d962f20cea2990aaaa78ae761
LuanVasco <devluangodoy@gmail.com>
2021-06-09 21:38:54 -0300

    Estilizando inputs

--- src/App.vue
+++ src/App.vue
@@ -71,4 +71,22 @@ img {
   background: #65D;
   transform: scale(1.1);
 }
+
+input, textarea {
+  border-radius: 4px;
+  border: 1px solid #fff;
+  padding: 15px;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  transition: all .3s;
+  font-size: 1rem;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  margin-bottom: 15px;
+}
+
+input:hover, textarea:hover
+input:focus, textarea:focus {
+  outline: none;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  border-color: #87F;
+}
 </style>
--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <form action="">
-      <input type="text" name="busca" id="busca" v-model="busca">
+      <input type="text" name="busca" id="busca" v-model="busca" placeholder="Buscar...">
       <input type="submit" id="lupa" value="buscar" @click.prevent="buscarProdutos">
     </form>
   </section>
@@ -23,5 +23,33 @@ export default {
 </script>
 
 <style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative; 
+}
 
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+
+#lupa {
+  position:absolute;
+  top: 0;
+  right: 0;
+  width:62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  box-shadow: none;
+}
 </style>

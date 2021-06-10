1b562d715857311a90748f046c3a839d8d8dbe18
LuanVasco <devluangodoy@gmail.com>
2021-06-09 21:20:12 -0300

    Adicionando condicoes para busca e css cards

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,11 +1,21 @@
 <template>
   <section class="produtos-container">
-    <article v-for="produto in produtos" :key="produto.id">
-      <img v-if="produto.fotos " :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
-      <p class="preco">{{ produto.preco }}</p>
-      <h2 class="title">{{ produto.nome }}</h2>
-      <p>{{ produto.descricao }}</p>
-    </article>
+    <div v-if="produtos && produtos.length" class="produtos">
+      <article v-for="produto in produtos" :key="produto.id" class="produto">
+        <router-link to="/"> 
+          <img v-if="produto.fotos " :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="title">{{ produto.nome }}</h2>
+          <p>{{ produto.descricao }}</p>
+        </router-link>
+      </article>
+    </div>
+    <div class="sem-resultados" v-else-if="produtos && produtos.length === 0">
+      <p>Busca sem resultados. Tente buscar outro termo</p>
+    </div>
+    <div v-else>
+      <p>Carregando...</p>
+    </div>
   </section>
 </template>
 
@@ -45,5 +55,45 @@ export default {
 </script>
 
 <style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px
+}
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #FFF;
+  border-radius: 4px;
+  transition:  all 0.2s;
+}
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position:relative;
+  z-index: 1;
+}
 
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: 700;
+}
+
+.sem-resultados {
+  text-align: center;
+}
 </style>

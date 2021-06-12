88f273ca44ab4135126545a6dddceda82f2cfc24
LuanVasco <devluangodoy@gmail.com>
2021-06-12 20:54:35 -0300

    Criando POST do formulario

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -4,7 +4,7 @@
     <transition mode="out-in">
       <button v-if="!criar" class="btn" @click="criar = true">Criar Conta</button>
       <UsuarioForm v-else>
-        <button class="btn btn-user">Criar Usuário</button>
+        <button class="btn btn-user" @click.prevent="criarUsuario">Criar Usuário</button>
       </UsuarioForm>
     </transition>
   </section>  
@@ -20,6 +20,17 @@ export default {
   data() {
     return {
       criar: false,
+    }
+  },
+  methods: {
+    async criarUsuario() {
+      try{
+        await this.$store.dispatch("criarUsuario", this.$store.state.usuario)
+        await this.$store.dispatch("getUsuario", this.$store.state.usuario.email)
+        this.$router.push({name: "usuario"})
+      } catch(e) {
+        console.log(e)
+      }
     }
   }
 }

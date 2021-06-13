57ef8c9626af6a54be6ca1874886d3c4de9b1850
LuanVasco <devluangodoy@gmail.com>
2021-06-13 11:49:51 -0300

    Cadastrando produtos na area do usuario

--- src/components/ProdutoAdicionar.vue
+++ src/components/ProdutoAdicionar.vue
@@ -1,13 +1,55 @@
 <template>
-  <p>Produto adicionar</p>
+  <form class="adicionar-produto">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome" v-model="produto.nome">
+    <label for="preco">Preço</label>
+    <input type="text" name="preco" id="preco" v-model="produto.preco">
+    <label for="fotos">Fotos</label>
+    <input type="file" name="fotos" id="fotos" ref="fotos">
+    <label for="descricao">Descrição</label>
+    <textarea name="descricao" id="descricao" v-model="produto.descricao"></textarea>
+    <input type="button" value="Adicionar Produto" @click.prevent="adicionarProduto" class="btn">
+  </form>
 </template>
 
 <script>
+import { api } from "@/services"
 export default {
   name: "ProdutoAdicionar",
+  data() {
+    return {
+      produto: {
+        nome: "",
+        preco: "",
+        descricao: "",
+        fotos: null,
+      }
+    }
+  },
+
+  methods: {
+    formatarProduto() {
+      this.produto.usuario_id = this.$store.state.usuario.id
+    },
+    adicionarProduto() {
+      this.formatarProduto()
+      api.post("/produto", this.produto).then(() => {
+        this.$store.dispatch("getUsuarioProdutos")
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
+.adicionar-produto {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  align-items: center;
+  margin-bottom: 60px
+}
 
+.btn {
+  grid-column: 2;
+}
 </style>
